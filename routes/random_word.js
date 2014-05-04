2236d4551ad05d794eb2bd4df90a8f509cdc00fb
Angus Mark <ngsmrk@gmail.com>
2014-05-04 18:41:18 +0100

    Kind of working - need to work out how to chain callbacks.

--- routes/random_word.js
+++ routes/random_word.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var wordnik = require('wordnik');
+
 /* GET random_word listing. */
 router.get('/', function(req, res) {
-    random_word = 'perspicacious';
+
+    var wn = new wordnik({
+        api_key: ''
+    });
+
+    var random_word = 'default';
+    var func = function(e, word) { console.log(word); console.log(e); };
+
+    wn.randomWord(func);
+
     res.json({random_word: random_word});
 });
 
